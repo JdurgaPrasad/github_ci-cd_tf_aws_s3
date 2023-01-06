ab5d7197b804c8f48071767242d50894866b039f
JdurgaPrasad <durgaprasadj2009@gmail.com>
2023-01-06 23:11:16 +0530

    initial changes

---
+++ src/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = "cc-tf-state-backend-ci-cd-2023"
+}
